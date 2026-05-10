f036eb446c352c728e36b19f4d79f26d8d4ed6f2
Tyler <tyler.t@GCC-TylerT.local>
2026-05-10 14:09:08 -0700

    update

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+
 from src import create_app
 
 
@@ -5,4 +7,7 @@ app = create_app()
 
 
 if __name__ == "__main__":
-	app.run(debug=True, host="0.0.0.0", port=5123)
+	port = int(os.environ.get("PORT", 5123))
+	debug_env = os.environ.get("FLASK_DEBUG")
+	debug = debug_env.lower() in {"1", "true", "yes", "on"} if debug_env is not None else "PORT" not in os.environ
+	app.run(debug=debug, host="0.0.0.0", port=port)
